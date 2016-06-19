111649568bc6285137b1c270c05923ce431dfe4e
Barbara <bnerness@gmail.com>
2016-06-18 21:49:50 -0700

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
+    
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -10,24 +12,38 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
-        # Babs heard about a cool new online to-do app. She goes to check out its
-        # homepage
+        # Babs heard about a cool new online to-do app. 
+        #She goes to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices that the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
         # She types "Start boxing again" into a text box 
+        inputbox.send_keys('Start boxing again') 
+
         # When she hits enter, the page updates, and now it lists 
-        
         # "1: Start boxing again" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Start boxing again' for row in rows)
+        )
 
         # There's still a text box inviting her to add another item
         # She enters "Buy nicer goggles for swimming"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
